7c2fae0fc87700cd9182b1691cf8453699dd787e
DDTim <NIKITA13.102010@mail.ru>
2018-01-13 01:51:48 +0700

    Глагле, кнопка тарифы, о нас, редиз 2.0++

--- web/controllers/TariffController.js
+++ web/controllers/TariffController.js
@@ -23,11 +23,15 @@ class TariffController{
         var tariffs =this.getAllTariffs();
         var tariffsView=document.getElementById("tariffs");
         var result="";
+        tariffsView.innerHTML = "result5";
         for(var i=0; i<tariffs.length;i++)
         {
-            result+= "<div class='row'>\n\
-            <div class='alert alert-success'>"+tariffs[i].t_name+
-            "</div></div>"
+            result+= "<div  style='border: 2px solid red; margin-top: 3%; padding: 2%; '>\n\
+            <div><h4>"+tariffs[i].t_name+"</h4></div> \n\
+            <div>Цена: "+tariffs[i].r_cost+"</div>\n\
+            <div>Доступное количество посещений: "+tariffs[i].t_number_visits+"</div>\n\
+            <div>Доступное количество дней: "+tariffs[i].t_number_days+"</div>\n\
+            </div>"
         }
         tariffsView.innerHTML = result;
     }
